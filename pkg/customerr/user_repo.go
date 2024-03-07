c7b619f6b5347eeda7e42df7a59b6cf4f053dab3
Nicolashka <nperewerzew2005@yandex.ru>
2024-03-07 22:25:15 +0300

    ADD err_normalizer

--- pkg/customerr/user_repo.go
+++
@@ -1,7 +0,0 @@
-package customerr
-
-const (
-	UserRollbackError = Error("error on rollback in user repo")
-	UserScanError     = Error("error on scan in user repo")
-	UserCommitError   = Error("error on committing tx in user repo")
-)
